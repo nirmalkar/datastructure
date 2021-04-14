91479154ba9083776cc5109f673073ca72d81d62
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-04-14 17:27:45 +0530

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+console.log(bubbleSort([1, 3, 2, 6, 5, 4]));
