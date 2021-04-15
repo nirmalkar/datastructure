4b9b7e20769cdf97fc9e2c60f48f6c546e66ebf0
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-04-15 16:23:36 +0530

    selection sort optimization

--- selectionSort.js
+++ selectionSort.js
@@ -4,22 +4,24 @@ const selectionSort = (arr) => {
     //setting the first element of the array as current index for minimum value
     let currMinimum = i;
     for (let j = i + 1; j < arr.length; j++) {
-      console.log(arr[currMinimum], arr[j]);
       // if next element in array is less than the current minimum value then setting index of it to current minimum
       if (arr[j] < arr[currMinimum]) {
         currMinimum = j;
       }
     }
     // storing value of current index in temp variable
-    let temp = arr[i];
-    // assigning current minimum value to the current index
-    arr[i] = arr[currMinimum];
-    // assigning value of currMinimum index the temp (which is was the current value)
-    arr[currMinimum] = temp;
+    if (i !== currMinimum) {
+      console.log(i, currMinimum);
+      let temp = arr[i];
+      // assigning current minimum value to the current index
+      arr[i] = arr[currMinimum];
+      // assigning value of currMinimum index the temp (which is was the current value)
+      arr[currMinimum] = temp;
+    }
     console.log("*************");
   }
   return arr;
 };
 
-const sortedValue = selectionSort([3, 2, 4, 1, 5]);
+const sortedValue = selectionSort([0, 1, 2, 3, 2, 4, 1, 5]);
 console.log(sortedValue);
