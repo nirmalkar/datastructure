40f8068354504d1fc9020d2a092346b02905b6f7
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-05-16 00:56:20 +0530

    factorial of 0 is 1

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,4 +1,6 @@
 function calculateFactorial(num) {
+  //factorial of 0 is always 1
+  if (num === 0) return 1;
   function helper(num) {
     if (num === 1) return 1;
     return num * helper(num - 1);
