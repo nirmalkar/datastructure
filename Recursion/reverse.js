d082031153363f66b457fbc78ddb3e75103cceec
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-05-23 14:35:48 +0530

    str reverse using recursion

---
+++ Recursion/reverse.js
@@ -0,0 +1,17 @@
+function reverse(str) {
+  const reversedStr = [];
+  function helper(num) {
+    console.log(num);
+    if (num === 0) {
+      reversedStr.push(str[num]);
+      return reversedStr.join("");
+    }
+    reversedStr.push(str[num]);
+    return helper(num - 1);
+  }
+  return helper(str.length);
+}
+
+const rsOne = reverse("awesome"); // 'emosewa'
+const rsTwo = reverse("rithmschool"); // 'loohcsmhtir'
+console.log(rsOne, rsTwo);
