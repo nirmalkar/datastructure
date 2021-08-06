eb8afb55eab258d30daacfe8f711474cc17cd38c
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-06 10:32:48 +0530

    print odd recursion

--- Recursion/collectOdd.js
+++ Recursion/collectOdd.js
@@ -18,3 +18,22 @@ function collectOdd(arr) {
 const oddValuesArr = collectOdd([1, 3, 4, 2, 5, 6, 7, 9]);
 
 console.log(oddValuesArr);
+
+//test 6th Aug 2021
+function collectOdd(arr) {
+  const oddNumsArr = []
+  function helperFunc(arr, times) {
+    console.log(times)
+    if (times === 0) {
+      return;
+    }
+if(arr[times] % 2 !== 0){
+  oddNumsArr.push(arr[times])
+}
+    return helperFunc(arr, times - 1);
+  }
+  helperFunc(arr, arr.length - 1)
+  console.log(oddNumsArr)
+}
+
+const oddValues = collectOdd([1, 3, 4, 2, 5, 6, 7, 9]);
