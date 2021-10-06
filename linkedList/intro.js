c75d06c5d509eddf11bcdf2f86ba2217cc8c93ff
nirmalkar <hemantnirmalkar17@gmail.com>
2021-10-06 10:21:53 +0530

    linked list push

--- linkedList/intro.js
+++ linkedList/intro.js
@@ -6,37 +6,5 @@ class Node {
     }
 }
 
-const first = new Node("hello")
-// console.log(first);
-first.next = new Node("world")
-// console.log(first);
 
-class SinglyLinkedList{
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.length = 0 
-    }
-    push(val){
-        let newNode = new Node(val)
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        }else{
-            this.tail.next = newNode
-            this.tail = newNode
-        }
-        this.length++;
-        return this
-    }
-}
-
-const list = new SinglyLinkedList()
-list.push(1)
-list.push("Hi")
-list.push("There")
-list.push("How")
-list.push("are")
-list.push("you")
-list.push("?")
-console.log(list);
+export default Node
