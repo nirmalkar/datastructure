571189baa7149edeaa3afa2cdd9213e4e1da871f
nirmalkar <hemantnirmalkar17@gmail.com>
2021-12-19 12:49:03 +0530

    Binary search tree

---
+++ Tree/BFS.js
@@ -0,0 +1,22 @@
+BinarySearchTree.prototype.BFS = function () {
+    let node = this.root,
+        data = [],
+        queue = [];
+    queue.push(node);
+    while (queue.length) {
+        node = queue.shift();
+        data.push(node.value);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return data;
+};
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.BFS());
